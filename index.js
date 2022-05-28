2a80e01a730b312271c41b8c6a3903d8f3581f89
Nikhil Jose <nikhiljose12345@gmail.com>
2022-05-28 13:08:12 +0530

    v1 release

--- index.js
+++ index.js
@@ -16,14 +16,21 @@ function submit(){
     axios.post(`${baseURL}/new`,{
         "name":document.getElementById("name").value,
         "email":document.getElementById("email").value,
-        "answer":document.getElementById("answer").value,
+        "answer":document.getElementById("answer").value.toLowerCase(),
         "qid":qid
       }).then(res=>{
         console.log(res.data)
-        document.getElementById("result").innerHTML=res.data.message        
+        document.getElementById("result").innerHTML=res.data.message
+        alert(res.data.message)
+        
+        if(res.data["id"]!=undefined){
+            window.location=`https://kiw.mulearn.org/user?id=${res.data.id}`
+        }
+        
     })
     .catch(err=>{
         console.log(err)
-        document.getElementById("result").innerHTML=`<h4>Some Error Occured</h4>${err.response.data}`
+        document.getElementById("result").innerHTML=`<h4>Some Error Occured</h4>${err.response.data.message}`
+        alert(`Error: ${err.response.data.message}`)
     })
 }
